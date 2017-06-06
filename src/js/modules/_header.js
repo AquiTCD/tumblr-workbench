0736c631134ac8d7420414930fec9154249fd6fb
Aqui Tsuchida <aqui.tcd+dev@gmail.com>
2017-06-06 19:15:10 +0900

    Add amp-setting

--- src/js/modules/_header.js
+++ src/js/modules/_header.js
@@ -4,15 +4,18 @@
 // TODO:スクロールイベント頻度の間引きが必要
 $(function(){
   const offsetTop = $('.js-headerNav').offset().top;
+  const navHeight = $('.js-headerNav').height();
   let timer = null;
   $(window).scroll(function(){
     clearTimeout( timer );
     timer = setTimeout(function() {
       let scrollAmount = $(document).scrollTop();
       if (scrollAmount > offsetTop){
-        $('.js-headerNav').addClass('is-scrolled')
+        $('.js-headerNav').addClass('is-scrolled');
+        $('.js-headerDescription').css('margin-top', navHeight)
       } else {
-        $('.js-headerNav').removeClass('is-scrolled')
+        $('.js-headerNav').removeClass('is-scrolled');
+        $('.js-headerDescription').css('margin-top', '0')
       }
     }, 1 );
   })
